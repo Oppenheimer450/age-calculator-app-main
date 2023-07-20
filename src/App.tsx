ccb81583f9df3472ddf9555a23db2d38ddd2be36
Holler, Patrik <hollepat@fel.cvut.cz>
2023-07-20 13:07:31 +0200

    project structure changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Your Name Here</a>.
+        . Coded by <a href="#">Patrik Holler</a>.
       </div>
     </>
   );
